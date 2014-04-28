28dad8e15468e94e0746569d83bd4938c479aacb
Dave Albert <dave.albert@publicisdhealthcare.ie>
2014-04-28 14:38:18 +0100

    starts with recent then adds new posts to top of list

--- app/js/facebook.js
+++ app/js/facebook.js
@@ -1,21 +1,25 @@
-function display_feed(newdata) {
-   console.log('display_feed',newdata);
-
-   var feed_data =  '';
-   newdata.data.forEach(function (item) {
-      feed_data += '<li>' + item.message + '</li>';
-   });
-   document.getElementById('feed-data').innerHTML = feed_data;
-}
-
 var FB = {
+
+   item_description: function(item) {
+      var msg = ['message', 'story', 'caption'];
+      var return_text = "";
+
+      msg.forEach(function(m){
+        if (typeof item[m] !== 'undefined') {
+           return_text = item[m];
+        }
+      });
+
+      return return_text;
+   },
+
    get_access_token: function () {
       try {
          var params = document.location.href.split('#')[1];
          params.split('&').forEach(function(p){
            var el = p.split('=');
            if (el[0] === 'access_token') {
-             facebook_settings.access_token = el[1];
+             FB.access_token = el[1];
            }
          });
       } catch(e) {
@@ -25,15 +29,40 @@ var FB = {
 
       return true;
    },
+
+   next_url: function() {
+      return FB.paging_url || 'https://graph.facebook.com/me/home?access_token=' + FB.access_token;
+   },
+
+   update_status: function(status) {
+      document.getElementById('login').innerHTML = status;
+   },
+
    update_feed: function() {
+      FB.update_status('Updating...')
       var xhr = new XMLHttpRequest();
       xhr.onreadystatechange = function() {
          if (xhr.readyState == 4) {
-            display_feed(JSON.parse(xhr.response));
+            FB.display_feed(JSON.parse(xhr.response));
          }
       }
-      xhr.open('GET', 'https://graph.facebook.com/me/home?access_token=' + facebook_settings.access_token, true);
+      xhr.open('GET', FB.next_url(), true);
       xhr.send(null);
-   }
-}
+   },
 
+   display_feed: function (newdata) {
+      console.log('display_feed',newdata);
+
+      var feed_data =  '';
+      newdata.data.forEach(function (item) {
+         feed_data += '<li>' + FB.item_description(item) + '</li>';
+      });
+      document.getElementById('feed-data').innerHTML = feed_data + document.getElementById('feed-data').innerHTML;
+      if (newdata && newdata.paging && newdata.paging.previous) {
+         FB.paging_url = newdata.paging.previous;
+      }
+
+      FB.update_status('Waiting...')
+   }
+
+}
--- app/js/main.js
+++ app/js/main.js
@@ -13,13 +13,12 @@ var facebook_login_url = 'https://www.facebook.com/dialog/oauth?response_type=to
                            facebook_settings.app_id +
                            '&redirect_uri=http%3A%2F%2Flocalhost.local&scope=read_stream';
 
+
 window.onload = function(){
    if (FB.get_access_token()) {
-      //setInterval(FB.update_feed, 5000);
-      document.getElementById('login').innerHTML = facebook_settings.access_token;
       FB.update_feed();
+      window.facebook_timer = setInterval(FB.update_feed, 5000);
    } else {
-      console.log('show login');
       document.getElementById('login-link').href = facebook_login_url;
    }
 
